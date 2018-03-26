b87d51c0ea96e77837594cc304c29e26b67213ea
Daisuke Araki <daisukeArk@users.noreply.github.com>
2018-03-26 11:23:46 +0900

    [add]メインファイル追加

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as Collections from './lib/collections';
+import * as Conditions from './lib/conditions';
+import * as Models from './lib/models';
+import * as Services from './lib/services';
+
+/**
+ * メイン
+ */
+export {
+  Collections,
+  Conditions,
+  Models,
+  Services
+};
